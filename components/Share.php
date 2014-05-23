b8030e22e6b206b24a33a29be44a07ee66ce72ed
Hariadi Hinta <diperakui@yahoo.com>
2014-05-23 10:45:12 +0800

    Remove unused method

--- components/Share.php
+++ components/Share.php
@@ -6,43 +6,25 @@ use Illuminate\Support\Facades\Request as Request;
 
 class Share extends ComponentBase
 {
-
     public function componentDetails()
     {
         return [
             'name'        => 'Share Component',
-            'description' => 'Outputs a social share button on a blog post post.'
+            'description' => 'Outputs a social share button on a page and blog post post.'
         ];
     }
 
-    public function facebook()
-    {
-        return Settings::get('facebook');
-    }
-
-    public function twitter()
-    {
-        return Settings::get('twitter');
-    }
-
-    public function googleplus()
-    {
-        return Settings::get('googleplus');
-    }
-
     public function onRun()
-{
-    // This code will be executed when the page or layout is
-    // loaded and the component is attached to it.
-    $this->addCss('/plugins/hariadi/share/assets/css/share.css'); 
+    {
+        $settings = Settings::instance();
 
-    $settings = Settings::instance();
+        $this->addCss('/plugins/hariadi/share/assets/css/share.css');
 
-    $this->page['facebook'] = $settings->facebook;
-    $this->page['twitter'] = $settings->twitter;
-    $this->page['googleplus'] = $settings->googleplus;
-      
-    $this->page['url'] = Request::url();
-}
+        $this->page['facebook'] = $settings->facebook;
+        $this->page['twitter'] = $settings->twitter;
+        $this->page['googleplus'] = $settings->googleplus;
+          
+        $this->page['url'] = Request::url();
+    }
 
 }
